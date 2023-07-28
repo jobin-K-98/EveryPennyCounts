536032114970b0299233b5323fc6a23e3eded404
Gokul Saji Kumar <gokulskumar21@gmail.com>
2023-07-27 23:33:50 -0400

    Add project files.

---
+++ EveryPennyCounts/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EveryPennyCounts.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options) 
+        { }
+
+        //Tables are defined in the Db Context
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ EveryPennyCounts/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EveryPennyCounts.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+
+        //CategoryId
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public String? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
